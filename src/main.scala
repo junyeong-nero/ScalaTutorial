db16aa2a21e59635cb42cb6f867c5275b01f8d7a
junyeong-nero <magicwho@unist.ac.kr>
2021-09-14 22:15:57 +0900

    [add] sum, length, map, toString, concat, remove, add

--- src/main.scala
+++ src/main.scala
@@ -20,6 +20,10 @@ object main {
 	}
 
 	def main(args: Array[String]): Unit = {
+		FunctionalProgramming()
+	}
+
+	def ClassTest(): Unit = {
 		val e: Entity = new Entity();
 		e.vel = Array(0, 1, 0)
 
@@ -27,10 +31,6 @@ object main {
 		a.animalType = Type.ANIMAL.SHEEP
 		a.pos = Array(1, 2, 3)
 		log(a.toString)
-
-		TupleTest()
-		MapTest()
-
 	}
 
 	def IfTest(): Unit = {
@@ -65,4 +65,73 @@ object main {
 
 		mutable.HashMap
 	}
+
+	sealed trait IntList
+	case class Nil() extends IntList
+	case class Cons(head: Int, tail: IntList) extends IntList
+
+	def FunctionalProgramming(): Unit = {
+		println(sum(10))
+		val test = Cons(10, Cons(9, Nil())) // 10::9::nil
+		val test2 = Cons(1, Cons(2, Cons(3, Nil()))) // 1,2,3
+		val test3 = Cons(4, Cons(5, Cons(6, Nil()))) // 4,5,6
+		println(s"test1 : ${toString(test)}")
+		println(s"test2 : ${toString(test2)}")
+		println(s"test3 : ${toString(test3)}")
+		println(length(test))
+
+		println(s"test3 : ${toString(remove(test3, 2))}")
+		println(s"test3 : ${toString(add(test3, 2, 100))}")
+	}
+
+
+	def sum(num: Int): Int =
+		if (num == 0)
+			0
+		else
+			num + sum(num - 1)
+
+	def length(list: IntList): Int =
+		list match {
+			case Nil() => 0
+			case Cons(head, tail) => 1 + length(tail)
+		}
+
+	def map(list: IntList, func: Int => Int): IntList =
+		list match {
+			case Nil() => Nil()
+			case Cons(head, tail) => Cons(func(head), map(tail, func))
+		}
+
+	def toString(list: IntList, str: String = ""): String =
+		list match {
+			case Nil() => str.substring(0, str.length - 2)
+			case Cons(head, tail) => toString(tail, str + head.toString + ", ")
+		}
+
+	def concat(first: IntList, second: IntList): IntList =
+		first match {
+			case Nil() => second
+			case Cons(head, tail) => Cons(head, concat(tail, second))
+		}
+
+	def add(list: IntList, index: Int, element: Int): IntList =
+		list match {
+			case Nil() => Nil()
+			case Cons(head, tail) =>
+				if (index == 0)
+					Cons(element, list)
+				else
+					Cons(head, add(tail, index - 1, element))
+		}
+
+	def remove(list: IntList, index: Int): IntList =
+		list match {
+			case Nil() => Nil()
+			case Cons(head, tail) =>
+				if (index == 0)
+					remove(tail, index - 1)
+				else
+					Cons(head, remove(tail, index - 1))
+		}
 }
